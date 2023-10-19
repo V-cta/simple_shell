f16ab4d197fb300959f70200941b3dd1e8cf2a97
V-cta <victor.ngugi.edu@gmail.com>
2023-10-18 18:49:49 -0700

    Shell project compiled

--- main.c
+++ main.c
@@ -5,73 +5,68 @@
 
 /**
  * main - Entry point for the shell program.
- * @ac: The number of command-line arguments.
- * @argv: An array of strings containing the arguments.
  * Return: 0 on success, -1 on failure.
  */
 
-int main(int ac)
+int main(void)
 {
-	char *prompt = "(simple_shell) $ ";
-	char *prtdline = NULL, *prtdline_copy = NULL;
-	size_t n = 0;
-	ssize_t read_char;
-	const char *delim = " \n";
-	int num_tokens = 0;
-	char *token;
-	int i;
-
-  /* declaring void variables */
-    (void)ac;
+    char *prompt = "(simple_shell) $ ";
+    char *prtdline = NULL, *prtdline_copy = NULL;
+    size_t n = 0;
+    ssize_t read_char;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *token;
+    int i;
 
     /* create an infinite loop */
     while (1)
     {
-	    printf("%s", prompt);
-	    read_char = getline(&prtdline, &n, stdin);
-	    /*if getline function fails or reaches EOF or user use CTRL + D */
-	    if (read_char == -1)
-	    {
-		printf("You are about to exit shell....\n");
-		free(prtdline);
-		return (-1);
-	    }
-	    /* allocating space for a copy of the prtdline */
-	    prtdline_copy =  malloc(sizeof(char) * read_char + 1);
-	    if (prtdline_copy == NULL)
-	    {
-		    perror("tsh: memory allocation error");
-		    return (-1);
-	    }
-	    /*copy prtdline to prtdline_copy*/
-	    strcpy(prtdline_copy, prtdline);
-	    /********** split the string (ptrdline) into an array of words ********/
-	    /* calculate the total number of tokens */
-	    token = strtok(prtdline, delim);
-	    while (token != NULL)
-	    {
-		    num_tokens++;
-		    token = strtok(NULL, delim);
-	    }
+        printf("%s", prompt);
+        read_char = getline(&prtdline, &n, stdin);
+        /* if getline function fails or reaches EOF or user uses CTRL + D */
+        if (read_char == -1)
+        {
+            printf("You are about to exit shell....\n");
+            free(prtdline);
+            return (-1);
+        }
+        /* allocating space for a copy of the prtdline */
+        prtdline_copy = malloc(sizeof(char) * read_char + 1);
+        if (prtdline_copy == NULL)
+        {
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy prtdline to prtdline_copy */
+        strcpy(prtdline_copy, prtdline);
+        /********** split the string (prtdline) into an array of words ********/
+        /* calculate the total number of tokens */
+        token = strtok(prtdline, delim);
+        while (token != NULL)
+        {
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
 
-	    /*Allocated spacefor holding the array of strings */
-	    char **avg = malloc(sizeof(char *) * (num_tokens + 1));
-	    /* Storing each token in the argv array */
-	    token = strtok(prtdline_copy, delim);
-	    for (i = 0; token != NULL; i++)
-	    {
-		    avg[i] = malloc(sizeof(char) * (strlen(token) + 1));
-		    strcpy(avg[i], token);
-		    token = strtok(NULL, delim);
-	    }
-	    avg[i] = NULL;
-	    /* execute the command using cmdexe(argv);*/
-	    for (i = 0; avg[i] != NULL; i++)
-	    {
-		    free(avg[i]);
-	    }
-	    free(avg);
-	    free(prtdline_copy);
+        /* Allocated space for holding the array of strings */
+        char **avg = malloc(sizeof(char *) * (num_tokens + 1));
+        /* Storing each token in the argv array */
+        token = strtok(prtdline_copy, delim);
+        for (i = 0; token != NULL; i++)
+        {
+            avg[i] = malloc(sizeof(char) * (strlen(token) + 1));
+            strcpy(avg[i], token);
+            token = strtok(NULL, delim);
+        }
+        avg[i] = NULL;
+        /* execute the command using cmdexe(avg); */
+        for (i = 0; avg[i] != NULL; i++)
+        {
+            free(avg[i]);
+        }
+        free(avg);
+        free(prtdline_copy);
     }
 
     free(prtdline);
--- shell.c
+++ shell.c
@@ -4,71 +4,68 @@
 #include <unistd.h>
 #include "main.h"
 
-int main(int ac)
+int main(void)
 {
-	char *prompt = "(simple_shell) $ ";
-	char *prtdline = NULL, *prtdline_copy = NULL;
-	size_t n = 0;
-	ssize_t read_char;
-	const char *delim = " \n";
+    char *prompt = "(simple_shell) $ ";
+    char *prtdline = NULL, *prtdline_copy = NULL;
+    size_t n = 0;
+    ssize_t read_char;
+    const char *delim = " \n";
 
-	/* declaring void variables */
-	(void)ac;
-
-    /* create an infinite loop*/
-	while (1)
-	{
-		printf("%s", prompt);
-		read_char = getline(&prtdline, &n, stdin);
-		/* if getline function fails or reaches EOF or user uses CTRL + D*/
-		if (read_char == -1)
-		{
-			printf("You are about to exit the shell....\n");
-			free(prtdline);
-			return (-1);
-		}
-		/* allocating space for a copy of the prtdline*/
-		prtdline_copy = malloc(sizeof(char) * read_char + 1);
-		if (prtdline_copy == NULL;
-				{
-				perror("tsh: memory allocation error");
-				return (-1);
-				}
-				/*copy prtdline to prtdline_copy*/
-				strcpy(prtdline_copy, prtdline);
-				/*split the string (prtdline) into an array of words*/
-				char **argv = NULL;
-				char *token;
-				int num_tokens = 0;
-				token = strtok(prtdline, delim);
-				while (token != NULL)
-				{
-				num_tokens++;
-				token = strtok(NULL, delim);
-				}
-				/*Allocate space for holding the array of strings*/
-				argv = malloc(sizeof(char *) * (num_tokens + 1));
-				/* Storing each token in the argv array*/
-				token = strtok(prtdline_copy, delim);
-				int i = 0;
-				while (token != NULL)
-				{
-					argv[i] = malloc(sizeof(char) * (strlen(token) + 1));
-					strcpy(argv[i], token);
-					token = strtok(NULL, delim);
-					i++;
-				}
-				argv[i] = NULL;
-				/* Execute the command*/
-				execmd(argv);
-				/*Free allocated memory*/
-				for (i = 0; i < num_tokens; i++)
-				{
-					free(argv[i]);
-				}
-				free(argv);
-				free(prtdline_copy);
-	}
-	free(prtdline);
-	return (0);
+    /* create an infinite loop */
+    while (1)
+    {
+        printf("%s", prompt);
+        read_char = getline(&prtdline, &n, stdin);
+        /* if getline function fails or reaches EOF or user uses CTRL + D */
+        if (read_char == -1)
+        {
+            printf("You are about to exit the shell....\n");
+            free(prtdline);
+            return (-1);
+        }
+        /* allocating space for a copy of the prtdline */
+        prtdline_copy = malloc(sizeof(char) * read_char + 1);
+        if (prtdline_copy == NULL)
+        {
+            perror("tsh: memory allocation error");
+            return (-1);
+        }
+        /* copy prtdline to prtdline_copy */
+        strcpy(prtdline_copy, prtdline);
+        /* split the string (prtdline) into an array of words */
+        char **argv = NULL;
+        char *token;
+        int num_tokens = 0;
+        token = strtok(prtdline, delim);
+        while (token != NULL)
+        {
+            num_tokens++;
+            token = strtok(NULL, delim);
+        }
+        /* Allocate space for holding the array of strings */
+        argv = malloc(sizeof(char *) * (num_tokens + 1));
+        /* Storing each token in the argv array */
+        token = strtok(prtdline_copy, delim);
+        int i = 0;
+        while (token != NULL)
+        {
+            argv[i] = malloc(sizeof(char) * (strlen(token) + 1));
+            strcpy(argv[i], token);
+            token = strtok(NULL, delim);
+            i++;
+        }
+        argv[i] = NULL;
+        /* Execute the command */
+        execmd(argv);
+        /* Free allocated memory */
+        for (i = 0; i < num_tokens; i++)
+        {
+            free(argv[i]);
+        }
+        free(argv);
+        free(prtdline_copy);
+    }
+    free(prtdline);
+    return (0);
 }
